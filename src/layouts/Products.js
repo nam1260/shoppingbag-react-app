aba8d1cbe3e5c176685a2907894b57a28ab6746e
wook <nam1260@gmail.com>
2021-12-19 02:47:18 +0900

    refactor(main): useState array 이름 명확하도록 productList로 변경

--- src/layouts/Products.js
+++ src/layouts/Products.js
@@ -1,9 +1,9 @@
 
 import React, { useState, useEffect, useCallback } from "react";
 import { useHistory } from 'react-router';
-import DataManager from "../../managers/DataManager"
-import ItemListComponent from "./component/ItemList"
-import Paginator from "./component/Paginator"
+import DataManager from "../managers/DataManager"
+import ItemListComponent from "../components/ItemList"
+import Paginator from "../components/Paginator"
 import styled from 'styled-components'
 import "./Product.css"
 
@@ -12,31 +12,27 @@ const ITEMS_PER_PAGE = 5;
 
 const Products = () => {
     const history = useHistory();
-    const [currentItems, setCurrentItems] = useState(null);
+    const [productList, setProductList] = useState([]);
+    const [currentProducts, setCurrentProducts] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
 
     const itemsPerPage = ITEMS_PER_PAGE;
-    const items = DataManager.getProductItemList();
 
     useEffect(()=>{
-        console.log("최초 랜더링 시에만 호출");
+        setProductList(DataManager.getProductItemList().sort((a,b)=>{return b.score-a.score}));
     },[]);
 
     useEffect(() => {
         const endOffset = itemOffset + itemsPerPage;
-        setCurrentItems(items.slice(itemOffset, endOffset));
-        setPageCount(Math.ceil(items.length / itemsPerPage));
-
-    }, [itemOffset, itemsPerPage]);
+        setCurrentProducts(productList.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(productList.length / itemsPerPage));
+    }, [itemOffset,productList]);
 
 
     // Invoke when user click to request another page.
     const handlePageClick = (event) => {
-        const newOffset = (event.selected * itemsPerPage) % items.length;
-        console.log(
-            `User requested page number ${event.selected}, which is offset ${newOffset}`
-        );
+        const newOffset = (event.selected * itemsPerPage) % productList.length;
         setItemOffset(newOffset);
     };
 
@@ -44,7 +40,7 @@ const Products = () => {
     return (
         <div className="layout">
             <section className="container">
-                <ItemListComponent itemList={currentItems}/>
+                <ItemListComponent itemList={currentProducts}/>
             </section>
             <section className="container">
                 <Paginator pageCount = {pageCount} onPageChange ={handlePageClick}/>
