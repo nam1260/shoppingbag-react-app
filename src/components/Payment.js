74906e9151215d46e82a2ee1edad75e36ceaf95a
wook <nam1260@gmail.com>
2021-12-19 20:31:25 +0900

    feat(main): 불필요 로그 제거

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -166,8 +166,6 @@ const PaymentDetail = ({products}) => {
         if(!result) {
           setCurrentCoupon(null);
         }
-
-        console.log("checkCouponValidation = "+result);
     }
 
     const calculatePaymentPrice = () =>{
