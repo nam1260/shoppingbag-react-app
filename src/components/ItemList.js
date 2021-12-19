6ce3e3a77dd8ad7e33c43f04cb13496105f3b521
wook <nam1260@gmail.com>
2021-12-20 01:41:14 +0900

    fix(main): itemlist 스타일 리펙토링

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import styled from 'styled-components'
 import {useDispatch, useSelector} from "react-redux"
 import {addCart, deleteCart, hasItem} from "../store/actions/index";
-
+import * as Strings from "../resources/Strings";
 
 const ItemList = styled.div`
   display: flex;
@@ -15,39 +15,85 @@ const ItemList = styled.div`
    flex-direction: column;
    margin-right: 20px;
   }
-  
-  & > div > button {
-     max-width: 300px;
-  }
-  
-  & > div > .couponInfo {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-  }
-  & > div > .itemInfo {
+   
+  .item-info {
     position: relative;
   }
   
-  .itemInfo > * {
+  .item-info > * {
      display: flex;
      margin-bottom: 5px;
   }
   
-  .itemInfo > img {
+  .item-info > img {
      margin-bottom: 10px;
      width: 300px;
      height: 300px;
   }
   
-  #item_name {
+  .item-info{
+    font-size: 16px;
+    text-align: left;
+    font-weight: 600;
+  }
+  #item-name {
     min-height: 40px;
   }
   
+  #item-price {
+    color: rgb(255,72,0);
+  }
+  
+  #item-score {
+    font-size: 14px;
+    color: rgb(80,80,80)
+  }
+  
+  .coupon-info {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    background: black;
+    color: white;
+  }
+  button {
+     max-width: 300px;
+  }
+  
+  
 `
 
+
+const ItemInfo = ({item}) => {
+
+    return (
+        <div>
+            <div className="item-info">
+                <img src={item.detail_image_url}/>
+                <div id="item-name">
+                    <span>{item.item_name}</span>
+                </div>
+                <div id="item-price">
+                    <span>{(item.price).toLocaleString()}원</span>
+                </div>
+                <div id="item-score">
+                    <img/>
+                    <span>{item.score}</span>
+                </div>
+            </div>
+
+            <div className="coupon-info">
+                <div>{item.availableCoupon !== false ? Strings.TEXT_DISCOUNT_PRODUCT : ""}</div>
+            </div>
+        </div>
+
+    )
+
+};
+
+
 //가격/사진/상품 제목
-const ItemListComponent = (props) => {
+const ItemListComponent = ({itemList}) => {
 
     const cart = useSelector(store=> store.cartReducer);
     const dispatch = useDispatch();
@@ -65,47 +111,31 @@ const ItemListComponent = (props) => {
     },[cart]);
 
     const onClickBtn = (e, item) => {
-        console.log(item);
-
         if(hasInCart(item)) {
             dispatch(deleteCart(item));
-            e.target.innerText = "장바구니 담기"
+            e.target.innerText = Strings.TEXT_ADD_CART
         }else {
             if(cart.length > 2) {
-                alert("장바구니 추가 가능 갯수는 최대 3개입니다");
+                alert(Strings.TEXT_LIMIT_CART_ALERT);
                 return ;
             }
             dispatch(addCart(item));
-            e.target.innerText = "장바구니 빼기"
+            e.target.innerText = Strings.TEXT_REMOVE_CART
         }
 
     };
 
     return (
-        props && props.itemList ? <ItemList className="items component">
+         itemList ? <ItemList className="items component">
             {
-                props.itemList.map((item, idx) => {
+                itemList.map((item) => {
                     return (
-                        <div id={"item_" + idx}>
-                            <div className="itemInfo">
-                                <img src={item.detail_image_url}/>
-                                <div id ="item_name">
-                                    <span>{item.item_name}</span>
-                                </div>
-                                <div id="price">
-                                    <span id="price">{(item.price).toLocaleString()}원</span>
-                                </div>
-                                <div id="score">
-                                    <div>하트</div>
-                                    <span>{item.score}</span>
-                                </div>
-                            </div>
+                        <div>
+                            <ItemInfo item={item}/>
+                            <button onClick={(e)=>{onClickBtn(e,item)}}>
+                                {hasInCart(item) ? Strings.TEXT_REMOVE_CART : Strings.TEXT_ADD_CART}
+                            </button>
 
-                            <div className="couponInfo">
-                                <span>{item.availableCoupon !==false ? "쿠폰 사용 가능" :""}</span>
-                            </div>
-
-                            <button onClick={(e)=>{onClickBtn(e,item)}}>{hasInCart(item) ? "장바구니 빼기" : "장바구니 담기"}</button>
                         </div>
                     )
                 })
